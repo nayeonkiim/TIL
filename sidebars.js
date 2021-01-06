c1c3142fc87f63fb60dec928565ed44b85b65d95
nayeonkiim <wkdrnsldk98@naver.com>
2021-01-06 14:52:22 +0900

    고급매핑 add

--- sidebars.js
+++ sidebars.js
@@ -1,6 +1,6 @@
 module.exports = {
   someSidebar: {
-    JPA: ["JPA/JPA1", "JPA/JPA2", "JPA/JPA3", "JPA/JPA4", "JPA/JPA5", "JPA/JPA6", "JPA/JPA7"],
+    JPA: ["JPA/JPA1", "JPA/JPA2", "JPA/JPA3", "JPA/JPA4", "JPA/JPA5", "JPA/JPA6", "JPA/JPA7", "JPA/JPA8"],
     Test: [
       {
         JUnit5: ["Test/JUnit5/JUnit51"],
