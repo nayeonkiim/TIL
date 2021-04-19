5c100a0180a0a8e07d297a2b5bdb513c109b6fa3
nayeonkiim <wkdrnsldk98@naver.com>
2021-04-19 12:37:29 +0900

    삭제

--- sidebars.js
+++ sidebars.js
@@ -31,7 +31,6 @@ module.exports = {
         다시풀자_구현: ["Algorithm/다시풀자구현/implement1"],
       }
     ],
-    DesignPatterns: ["DesignPatterns/design1", "DesignPatterns/design2", "DesignPatterns/design3", "DesignPatterns/design4", "DesignPatterns/design5", "DesignPatterns/design6", "DesignPatterns/design7"],
     Guides: [
       {
         Docusaurus: ["doc1", "doc2", "doc3"],
