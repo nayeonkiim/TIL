0b1c47beb0ae8d3c31142d6e9e040761946f1810
nayeonkiim <wkdrnsldk98@naver.com>
2021-06-30 14:58:16 +0900

    스트래티지 패턴

--- sidebars.js
+++ sidebars.js
@@ -32,7 +32,7 @@ module.exports = {
       }
     ],
     AWS: ["AWS/AWS1"],
-    DesignPattern: ["DesignPattern/design1"],
+    DesignPattern: ["DesignPattern/design1","DesignPattern/design2"],
     Guides: [
       {
         Docusaurus: ["doc1", "doc2", "doc3"],
