dee9448fa649a5842fe247db9a9c264e731ba309
nayeonkiim <wkdrnsldk98@naver.com>
2021-07-01 20:43:18 +0900

    옵저버 패턴

--- sidebars.js
+++ sidebars.js
@@ -32,7 +32,7 @@ module.exports = {
       }
     ],
     AWS: ["AWS/AWS1"],
-    DesignPattern: ["DesignPattern/design1","DesignPattern/design2"],
+    DesignPattern: ["DesignPattern/design1","DesignPattern/design2","DesignPattern/design3"],
     Guides: [
       {
         Docusaurus: ["doc1", "doc2", "doc3"],
